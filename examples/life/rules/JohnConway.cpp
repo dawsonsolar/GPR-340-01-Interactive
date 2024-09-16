e71895c7a844128eeeaf2aea5f3be26ff86d6e38
Dawson Jackson <dawson.jackson@mymaik.champlain.edu>
2024-09-15 20:42:04 -0400

    JohnConway.cpp Implemented, Starting on HexagonGameOfLife.cpp

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -2,10 +2,33 @@
 
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
-  // todo: implement
+  for (int y = 0; y < world.SideSize(); y++)
+  {
+    for (int x = 0; x < world.SideSize(); x++)
+    {
+      Point2D tempPoint = Point2D(x, y);
+      if (world.Get(tempPoint))
+      {
+        if (CountNeighbors(world, tempPoint) < 2 || CountNeighbors(world, tempPoint) > 3)
+          world.SetNext(tempPoint, false);
+        else
+          world.SetNext(tempPoint, true);
+      }
+    }
+  }
+  world.SwapBuffers();
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
-  // todo: implement
-  return 0;
+  int count = 0;
+  for (int y = -1; y <= 1; y++)
+  {
+    for (int x = -1; x <= 1; x++)
+    {
+      Point2D tempPoint = Point2D((point.x + x) % world.SideSize(), (point.y + y) % world.SideSize());
+      if (tempPoint != point && world.Get(tempPoint))
+        count++;
+    }
+  }
+  return count;
 }
